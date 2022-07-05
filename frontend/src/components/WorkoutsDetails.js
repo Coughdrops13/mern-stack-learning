48ad7b505eeaede6b8d370a157ad92cf875da55a
Connor Roberts <connoobets@gmail.com>
2022-07-05 10:53:04 -0600

    create delete workout path in the frontend

--- frontend/src/components/WorkoutsDetails.js
+++ frontend/src/components/WorkoutsDetails.js
@@ -1,12 +1,40 @@
+import { useWorkoutsContext } from "../hooks/use-workoutsContext";
+
 const WorkoutsDetails = ({ workout }) => {
   // alternate way to destructure workout data from  props
   // const {workout} = props.workout;
+
+  const { dispatch } = useWorkoutsContext();
+
+  const clickHandler = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    console.log("JSON", json);
+
+    if (!response.ok) {
+      console.log("SOMETHING WRONG");
+    }
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
-      <p><strong>Load (kg): </strong>{workout.load}</p>
-      <p><strong>Reps: </strong>{workout.reps}</p>
+      <p>
+        <strong>Load (kg): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Reps: </strong>
+        {workout.reps}
+      </p>
       <p>{workout.createdAt}</p>
+      <span onClick={clickHandler}>Delete</span>
     </div>
   );
 };
--- frontend/src/store/WorkoutsProvider.js
+++ frontend/src/store/WorkoutsProvider.js
@@ -11,6 +11,14 @@ export const workoutsReducer = (state, action) => {
       return {
         workouts: [action.payload, ...state.workouts],
       }
+    case "DELETE_WORKOUT":  
+      return {
+        workouts: state.workouts.filter((workout) => workout._id !== action.payload._id)
+      }
+    case "UPDATE_WORKOUT":
+      return {
+        workouts: [...state.workouts, action.payload]
+      }
     default:
       return state;
   }
